45f81879ab9439e90a08c1cb9232701ce388a73e
batpeace <batpeace@outlook.com>
2015-12-30 12:51:34 +0100

    Update mybooklib.h

--- mybooklib.h
+++ mybooklib.h
@@ -16,7 +16,7 @@ typedef struct Cell { Book_t book;
                       struct Cell* pNext; 
                     } Cell_t;                   //Ogni elemento della BookList.Sinonimo distruct Cell.
  
-typedef struct BookList { Cell_t* pFirst; Cell_t *pLast; } BookList_t; 
+typedef struct BookList { Cell_t *pFirst; Cell_t *pLast; } BookList_t; 
 
                      
 Cell_t* allocBook();                            //Allocare memoria per una struct Cell.
@@ -24,8 +24,8 @@ void insHead();                                 //Inserire libro in cima alla li
 void insTail();                                 //Inserire libro in fondo alla lista.
 void deallocBook(Cell_t*);                      //Libera spazio in memoria.
 void printElem(Book_t*);                        //Stampare elementi di un libro dato il suo codice.
-void rmvHead(Cell_t*);                          //Eliminazione libro in cima alla lista. 
-void rmvTail(Cell_t*);                          //Eliminazione libro in fondo alla lista.
+void rmvHead(BookList_t*);                      //Eliminazione libro in cima alla lista. 
+void rmvTail(Cell_t *pLast);                    //Eliminazione libro in fondo alla lista.
 void printList(BookList_t a);                   //Stampo tutti i campi di tutta la lista. 
 void instructions();                            //Scegliere la funzione da svolgere.
 Cell_t* searchId();                             //Ricerca elemento attraverso il suo codice.
