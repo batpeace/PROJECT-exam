5408ef522b66fac3fa757d60bffd5a6e4f16c04f
batpeace <batpeace@outlook.com>
2016-01-06 16:17:30 +0100

    Update progexam.c

--- progexam.c
+++ progexam.c
@@ -5,41 +5,56 @@
 
 int main()
 { 
-  //(Cell_t).pLast = NULL;                            //Inizializza pLast.
  BookList_t Library;
  int choice;
  instructions();
  printf("\nChoice: "); 
  scanf("%d", &choice);
- while(choice!=5)
+ while(choice!=7)
   {
   switch (choice) 
      {
       case 1: 
-      printf("Enter a book:\n");
+      printf("Enter the book:\n");
       insTail(&Library);
       break;
       
       case 2:
+      Cell_t *ptr = Control(&Library);
       printf("Enter the id:\n");
-      Cell_t* p= searchId(&Library);
-       if(p) //sottointeso != NULL
-	printElem(&p->book);
-       else
-	printf("No id available.\n");
+      Cell_t *p = searchId(&Library);
+      if(p!=NULL)
+      printElem(&p->book);
+      else printf("No book with this id available.\n");
       break;
  
-      /* case 3: */
-      /* printf("Find the book from its id:\n"); */
-      /* searchId(); */
-      /* break; */
+      case 3: //Come il Case 2 ma stampa TRUE o FALSE.
+      printf("Enter the id to see if the book is in the list:\n");
+      Cell_t *ren = searchId(&Library);
+      if (ren != NULL)
+       printf("TRUE\n"); 
+      else printf("FALSE\n");
+      break;
       
       case 4:
       printf("Remove the last book of the list.\n");
       rmvTail(&Library);
       break;
-     }  
-  printf("\nChoice: "); scanf("%d", &choice);
-  } 
+      
+      case 5:
+      printf("Enter the book:\n");
+      insHead(&Library);
+      break;
+            
+      case 6:
+      printf("Remove the first book of the list.\n");
+      rmvHead(&Library);
+      break;
+     } 
+      
+      printf("\nChoice: "); 
+      scanf("%d", &choice);
+  
+  }    
  return 0;
-} 
+}   
