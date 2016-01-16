4c7c9ef2b8113931b9e26fe6fa817b2ed7ed3167
batpeace <batpeace@outlook.com>
2016-01-16 12:04:27 +0100

    Update progexam.c

--- progexam.c
+++ progexam.c
@@ -6,7 +6,6 @@
 int main()
 { 
  BookList_t Library;
- BookList_t *List;
  int choice;
  instructions();
  printf("\nChoice: "); 
@@ -16,97 +15,81 @@ int main()
    switch (choice) 
      {
       case 1: 
-      printf("Enter the book:\n");
-      insTail(&Library);
+      printf("\nEnter the book:\n");
+      insHead(&Library);
+      printf("Book added correctly\n");
       printList(&Library);
+      printf("\nList printed correctly.\n");
       break;
       
       case 2:
-      printf("Enter the id:\n");
-      Cell_t *p = searchId(&Library);
-      if(p != NULL)
-      printElem(&p->book);
-      else printf("No book with this id available.\n");
+      printf("\nEnter the book:\n");
+      insTail(&Library);
+      printf("Book added correctly\n");
+      printList(&Library);
+      printf("\nList printed correctly.\n");
       break;
- 
-      case 3: //Come il Case 2 ma stampa TRUE o FALSE.
-      printf("Enter the id to see if the book is in the list:\n");
-      Cell_t *ren = searchId(&Library);
-      if (ren != NULL)
-       printf("TRUE\n"); 
-      else printf("FALSE\n");
+      
+      case 3: 
+      printf("\nRemoving the first book of the list...\n");
+      rmvHead(&Library);
+      printf("Function worked correctly.\n");
+      printList(&Library);
+      printf("\nList printed correctly.\n");
       break;
       
       case 4:
+      printf("\nRemoving the last book of the list...\n");
       rmvTail(&Library);
-      printf("Book removed successfully.\n");
+      printf("Function worked correctly.\n");
+      printList(&Library);
+      printf("\nList printed correctly.\n");
       break;
       
-      case 5:
-      printf("Enter the book:\n");
-      insHead(&Library);
+      case 5: 
+      rmvId(&Library);
+      printf("Function worked correctly.\n");
+      printList(&Library);
+      printf("\nList printed correctly.\n");
       break;
             
       case 6:
-      printf("Removed the first book of the list.\n");
-      rmvHead(&Library);
+      printf("\nEnter the id to see if the book is in the list:\n");
+      Cell_t* Ptr = searchIdTrueFalse(&Library);
+      if(Ptr != NULL)
+      printf("Function worked correctly.\n");
       break;
       
-      case 7:
-      printList(&Library);
+      case 7: 
+      copyOut(&Library);
+      printf("Function worked correctly.\n");
       break;
       
       case 8:
-      printf("Enter the book's id you want to delete:\n");
-      Cell_t* Ptr = searchId(&Library);
-      Cell_t *Temp = List->pFirst;
-      if (Ptr != NULL)
-      while(Temp != Ptr)
+      copyIn(&Library);
+      printf("Function worked correctly.\n");
+      break;
+
+      case 9: 
+      printf("\nEnter the id:\n");
+      Cell_t *p = searchId(&Library);
+      if(p != NULL)
       {
-       Temp = Temp->pNext;
-       Temp->pNext = NULL;
-       free(Ptr);
-       Ptr = Temp;
-      }        
+       printf("Book found correctly.\n");
+       printElem(&p->book);
+      } 
+      else printf("No book with this id available.\n"); 
+      printf("\nFunction worked correctly.\n");
       break;
       
-      case 9: //Funzione che restituisce un libro. (copyIn)
-      printf("Insert the id of the book you are returning:\n");
-      Cell_t* Pointer = searchId(&Library);
-      if(Pointer != NULL)
-       if (Pointer->book.outLibrary == 0)
-        { 
-         printf ("No book with this id is out of the library.\n"); 
-        }
-      else if (Pointer->book.outLibrary > 0)
-      {
-       Pointer->book.inLibrary++;
-       Pointer->book.outLibrary--;
-       printf("Now we have %hd %s in library and %hd out library.\n", 
-               Pointer->book.inLibrary,
-               Pointer->book.title,
-               Pointer->book.outLibrary);
-      }
+      case 10:
+      printf("\nSearching for the list..\n");
+      printList(&Library);
+      printf("List printed correctly.\n");
       break;
-      
-      case 10: //Funzione che prende un libro in prestito. (copyOut).
-      printf("Insert the id of the book you are borrowing:\n");
-      Cell_t* Puntatore = searchId(&Library);
-      if(Puntatore != NULL)
-        if (Puntatore->book.inLibrary == 0)
-        {
-         printf("All the books with this id are out of the library.\n");
-        } 
-         else if (Puntatore->book.inLibrary > 0)
-         {
-          Puntatore->book.inLibrary--;
-          Puntatore->book.outLibrary++;
-          printf("Now we have %hd %s in library and %hd out library.\n", 
-                  Puntatore->book.inLibrary,
-                  Puntatore->book.title,
-                  Puntatore->book.outLibrary); }            
-      break;
+     
      } 
+      
       printf("\nChoice: "); 
       scanf("%d", &choice);
   }    
